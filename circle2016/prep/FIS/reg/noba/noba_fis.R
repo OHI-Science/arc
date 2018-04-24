052d5152c77d73d5613eb095b5f694c057c3b517
mburgass <m.burgass14@imperial.ac.uk>
2018-04-24 13:09:23 +0000

    add fmsy2 files

--- circle2016/prep/FIS/reg/noba/noba_fis.R
+++ circle2016/prep/FIS/reg/noba/noba_fis.R
@@ -33,3 +33,28 @@ cmsy_bbmsy <- plyr::ldply(cmsy_fits, function(x) {
 })
 cmsy_bbmsy$model <- "CMSY"
 write.csv(cmsy_bbmsy, "circle2016/prep/FIS/reg/catch_model_bmsy_reg/cmsy_bbmsy_reg.csv", row.names=FALSE)
+
+###Format CMSY data for toolbox######
+library(dplyr)
+library(tidyr)
+library(zoo)
+library(stringr)
+
+cmsy <- read.csv('circle2016/prep/FIS/reg/catch_model_bmsy_reg/cmsy_bbmsy_reg.csv') %>%
+  mutate(prior = 'constrained') %>%
+  filter(!is.na(bbmsy_mean))
+
+#comsir <- read.csv('prep/FIS/catch_model_bmsy/comsir_bbmsy.csv') %>%
+#mutate(prior = 'NA') %>%
+#filter(!is.na(bbmsy_mean))
+
+new_b_bmsy <- function(b_bmsy=constrained, method = "cmsy"){
+  b_bmsy <- b_bmsy %>%
+    dplyr::select(stock_id, year, bbmsy_mean, prior, model) %>%
+    arrange(stock_id, year) %>%
+    group_by(stock_id) %>%
+    mutate(mean_5year = rollmean(bbmsy_mean, 5, align="right", fill=NA))
+  write.csv(b_bmsy, sprintf('circle2016/prep/FIS/noba/meanbmsy/%s_b_bmsy_%s_mean5yrs_reg.csv', method, unique(b_bmsy$prior)), row.names=FALSE)
+}
+
+new_b_bmsy(cmsy, method="cmsy")
